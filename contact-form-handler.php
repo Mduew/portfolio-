8b94462092deb63d04b9476fa99e919d4957b1f7
Mzamo Sithole <118007494+Mduew@users.noreply.github.com>
2024-02-08 14:49:25 +0200

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -11,7 +11,7 @@ $email_body ="user name: $name.\n"
 "user email: $visitor_email.\n".
     "user message: $message.\n";
 
-$to = "geeksmadtech@gmail.com";
+$to = "stholemduew@gmail.com";
 $headers = "from: $email_from\r\n";
 $headers ="reply-to:$visitor_email\r\n";
 mail($to,$email_subject,$email_body,$headers);
